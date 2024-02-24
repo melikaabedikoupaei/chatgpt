4af88c20eb43cb9ff2dd3d54c2519ea5d3ae533b
mabediko <melika.abedikoupaei@ugent.be>
2024-02-24 14:35:39 +0100

    connect to chatgpt api

--- django_chatbot/chatbot/views.py
+++ django_chatbot/chatbot/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import openai
 
-# Create your views here.
+openai_api_key = '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'
+openai.api_key = openai_api_key
+
+def ask_openai(message):
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are an helpful assistant."},
+            {"role": "user", "content": message},
+        ]
+    )
+    
+    answer = response.choices[0].message.content.strip()
+    return answer
+
+def chatbot(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        message="first translate then answer this in english and answer like your text:.... ,response:......"+message
+        response = ask_openai(message)
+        return JsonResponse({'message': message, 'response': response})
+    return render(request, 'chatbot.html')
+
